2ba79d8ccac83171f9439ea1a7b03c56531ee9dc
lionel <lionel.brahami@gmail.com>
2018-09-21 22:17:11 +0800

    Fix typo

--- frenchtvdownload/downloader/HLSDownloader.py
+++ frenchtvdownload/downloader/HLSDownloader.py
@@ -122,7 +122,6 @@ class HLSStreamDownloader(object):
 
         # Download fragments and create the ts file
         logger.info("Start downloading fragments")
-        maxNbrFrag = 3
         try:
             i = 0
             while i < maxNbrFrag and not self.stopDownloadEvent.isSet():
