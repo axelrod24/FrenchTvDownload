f84a664259113874caf5f689e700b66002e9ef2b
Lionel <lionel.brahami@gmail.com>
2019-03-10 07:33:53 +0800

    tmp commit

--- frenchtvdownload/frontend/src/index.js
+++ frenchtvdownload/frontend/src/index.js
@@ -4,18 +4,37 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class UrlModel {
-    constructor(url, status) {
-        this.url = url ;
-        this.status = status ;
-    }
-} 
+// class UrlModel {
+//     constructor(url, status) {
+//         this.url = url ;
+//         this.status = status ;
+//     }
+// } 
+
+const VideoMetaData = (manifest, synopsis, progTitle, filename, mediaType, duration, videoId, drm, timeStamp, progName) => (
+    {manifest: "", 
+        synopsis: "", 
+        progTitle: "", 
+        filename: "",
+        mediaType: "", 
+        duration: "", 
+        videoId: "", 
+        drm: "", 
+        timeStamp: "", 
+        progName: ""})
+
+const UrlModel = (url, status, videoMetaData) => ({url: url, status: status, metadata: videoMetaData})
+
+var v = {
+    url: "the url", 
+    status: "pending"
+}
 
 var AllUrls =[
-    new UrlModel("https://www.france.tv/documentaires/art-culture/910287-la-petite-histoire-des-super-heros.html", "pending"),
-    new UrlModel("https://www.france.tv/documentaires/science-sante/910533-haut-le-corps.html", "done"),
-    new UrlModel("https://www.france.tv/documentaires/politique/874585-guyane-la-frontiere-invisible.html", "pending"),
-    new UrlModel("https://www.france.tv/documentaires/societe/911315-le-sexisme-en-politique-un-mal-dominant.html", "pending"),
+    UrlModel("https://www.france.tv/documentaires/art-culture/910287-la-petite-histoire-des-super-heros.html", "pending"),
+    UrlModel("https://www.france.tv/documentaires/science-sante/910533-haut-le-corps.html", "done"),
+    UrlModel("https://www.france.tv/documentaires/politique/874585-guyane-la-frontiere-invisible.html", "pending"),
+    UrlModel("https://www.france.tv/documentaires/societe/911315-le-sexisme-en-politique-un-mal-dominant.html", "pending"),
 ]
 
 
