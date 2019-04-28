9c073012940781c39b29e3ac139ac3e15c5c51a3
Lionel <lionel.brahami@gmail.com>
2019-04-28 09:14:27 +0800

    update list of test url

--- frenchtvdownload/build_database.py
+++ frenchtvdownload/build_database.py
@@ -12,8 +12,8 @@ ALL_VIDEO = [
     {"url": "https://www.france.tv/france-5/c-a-vous/c-a-vous-saison-10/942659-c-a-vous.html", "status": "pending"},
     {"url": "https://www.tf1.fr/tmc/quotidien-avec-yann-barthes/videos/quotidien-premiere-partie-29-mars-2019.html", "status": "pending"},
     {"url": "https://www.tf1.fr/tmc/quotidien-avec-yann-barthes/videos/quotidien-deuxieme-partie-29-mars-2019.html", "status": "pending"},
-    {"url": "https://www.arte.tv/fr/videos/048268-001-A/crimes-a-la-cour-des-medicis-1-2/", "status": "pending"},
-    {"url": "https://www.arte.tv/fr/videos/048268-002-A/crimes-a-la-cour-des-medicis-2-2/", "status": "pending"},
+    # {"url": "https://www.arte.tv/fr/videos/048268-001-A/crimes-a-la-cour-des-medicis-1-2/", "status": "pending"},
+    # {"url": "https://www.arte.tv/fr/videos/048268-002-A/crimes-a-la-cour-des-medicis-2-2/", "status": "pending"},
     {"url": "https://www.france.tv/france-5/c-a-vous/c-a-vous-saison-10/941649-c-a-vous.html", "status": "pending"},
     {"url": "https://www.france.tv/france-5/la-maison-france-5/937699-la-maison-france-5.html", "status": "pending"}
     ]
