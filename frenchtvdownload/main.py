b37f0fe78f8a3c67cd60cffa813aa5770a578e53
axelrod <axel.rod@mailinator.com>
2019-05-21 13:41:16 +0000

    change location on virtual env/activate_this

--- frenchtvdownload/main.py
+++ frenchtvdownload/main.py
@@ -1,9 +1,12 @@
-#!/usr/local/bin/python3.7
+#!/usr/bin/python3
 # # -*- coding:Utf-8 -*-
 
-activate_this = '/var/www/FlaskApp/FrTvDld/.venv/bin/activate_this.py'
+activate_this = '.venv/bin/activate_this.py'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
+
+import sys
+print(sys.path)
 
 #
 # Infos
