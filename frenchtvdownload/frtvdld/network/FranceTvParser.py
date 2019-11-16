16f542c01617bacd3f826f1d5e68cdfcbc18ff75
axelrod <axel.rod@mailinator.com>
2019-11-16 16:10:43 +0800

    support new video metadata endpoint and json

--- frenchtvdownload/frtvdld/network/FranceTvParser.py
+++ frenchtvdownload/frtvdld/network/FranceTvParser.py
@@ -20,33 +20,59 @@ from frtvdld.network.NetworkParser import NetworkParser, VideoMetadata
 
 logger = logging.getLogger(LOGGER_NAME)
 
+# class FranceTvVideoMetadata(VideoMetadata):
+#   def __init__(self, d):
+#     super().__init__(d)
+
+#   def parseMetadata(self):
+#     self._airDate = self._parseAirDate(self.get('diffusion')['date_debut'])  #\TODO LBR: add default date value
+#     self._progName = self.get('code_programme', 'default_prog_name')
+#     self._progTitle = self.get('sous_titre', 'default_prog_title')
+#     self._synopsis = self.get('synopsis', "no synopsis")
+#     self._videoId = self.get('videoId')
+
+#     # duration (default value to 0 if not present)
+#     timestr = self.get('duree', '0:0:0')
+#     self._duration = sum([a*b for a,b in zip([3600,60,1], [int(i) for i in timestr.split(":")])])
+
+#     for v in self.get('videos'):
+#         if v.get('format') == ' hls_v5_os':
+#             self._manifestUrl = v.get('url_secure')
+#             # metaData['drm'] = v['drm']
+#             self._mediaType = "hls"
+
+#     self._filename = "%s-%s" % (datetime.fromtimestamp(self._airDate).strftime("%Y%m%d"), self.normalizeProgTitle(self._progName))
+  
+#   def _parseAirDate(self, str_date):
+#     split_date = str_date.split(" ")[0].split('/')
+#     d = datetime.fromisoformat("%d-%02d-%02d" % (int(split_date[2]), int(split_date[1]), int(split_date[0])))
+#     return d.timestamp()
+
+
 class FranceTvVideoMetadata(VideoMetadata):
   def __init__(self, d):
     super().__init__(d)
 
   def parseMetadata(self):
-    self._airDate = self._parseAirDate(self.get('diffusion')['date_debut'])  #\TODO LBR: add default date value
-    self._progName = self.get('code_programme', 'default_prog_name')
-    self._progTitle = self.get('sous_titre', 'default_prog_title')
-    self._synopsis = self.get('synopsis', "no synopsis")
-    self._videoId = self.get('videoId')
-
-    # duration (default value to 0 if not present)
-    timestr = self.get('duree', '0:0:0')
-    self._duration = sum([a*b for a,b in zip([3600,60,1], [int(i) for i in timestr.split(":")])])
-
-    for v in self.get('videos'):
-        if v.get('format') == 'hls_v5_os':
-            self._manifestUrl = v.get('url')
-            # metaData['drm'] = v['drm']
-            self._mediaType = "hls"
+    vMeta =  self.get('video')
+    self._manifestUrl = vMeta.get('url')
+    self._mediaType = vMeta.get('format') 
+    self._duration = vMeta.get('duration') 
+    
+    pMeta = self.get('meta')
+    self._videoId = pMeta.get('id')
+    self._progName = pMeta.get('title', 'default_prog_name')
+    self._progTitle = pMeta.get('additional_title', 'default_prog_title')
+    self._airDate = self._parseAirDate(pMeta.get('broadcasted_at'))  #\TODO LBR: add default date value
+    # self._synopsis = self.get('synopsis', "no synopsis")
 
     self._filename = "%s-%s" % (datetime.fromtimestamp(self._airDate).strftime("%Y%m%d"), self.normalizeProgTitle(self._progName))
   
   def _parseAirDate(self, str_date):
-    split_date = str_date.split(" ")[0].split('/')
-    d = datetime.fromisoformat("%d-%02d-%02d" % (int(split_date[2]), int(split_date[1]), int(split_date[0])))
+    split_date = str_date.split("T")[0]
+    d = datetime.fromisoformat(split_date)
     return d.timestamp()
+
 
 class FranceTvParser(NetworkParser):
     """
@@ -62,7 +88,7 @@ class FranceTvParser(NetworkParser):
     REGEX_M3U8 = "/([0-9]{4}/S[0-9]{2}/J[0-9]{1}/[0-9]*-[0-9]{6,8})-"
     JSON_DESCRIPTION = "http://webservices.francetelevisions.fr/tools/getInfosOeuvre/v2/?idDiffusion=_ID_EMISSION_"
     JSON2_DESC="https://sivideo.webservices.francetelevisions.fr/tools/getInfosOeuvre/v2/?idDiffusion=_ID_EMISSION_"
-    JSON3_DESC="https://player.webservices.francetelevisions.fr/v1/videos/_ID_EMISSION_?country_code=FR&w=1920&h=1080&version=5.5.2&domain=www.france.tv&device_type=desktop&browser=chrome&browser_version=71&os=macos&os_version=10_13_6"
+    JSON3_DESC="https://player.webservices.francetelevisions.fr/v1/videos/_ID_EMISSION_?country_code=FR&w=1920&h=1080&device_type=desktop&browser=safari"
 
     def getListOfUrlCollection(self, url):
        # read the page and extract list of videoURL
@@ -77,11 +103,18 @@ class FranceTvParser(NetworkParser):
         return list_of_url
 
     def getVideoUrl(self, url):
-        # read the page and extract url of first video
-        page = self.fakeAgent.readPage(url)
-        parsed = BeautifulSoup(page, "html.parser")
-        videoUrlList = parsed.find_all("a", attrs={"class": "c-card-video__link"})
-        return urljoin(url, videoUrlList[0]["href"])
+      # read the page and extract url of first video
+      page = self.fakeAgent.readPage(url)
+      parsed = BeautifulSoup(page, "html.parser")
+      videoUrlList = parsed.find_all("a", attrs={"class": "c-card-video__link"})
+      return urljoin(url, videoUrlList[0]["href"])
+
+    def getSynopsis(self, page):
+      parsed = BeautifulSoup(page, "html.parser")
+      desc = parsed.find_all("meta", attrs={"property": "og:description"})
+      if len(desc) > 0:
+        return desc[0].attrs["content"]
+      return "no synopsis"
 
     def parsePage(self, url):
         # check if url point to the video page, if not get list of video URl one by one
@@ -101,14 +134,18 @@ class FranceTvParser(NetworkParser):
               raise FrTvDwnPageParsingError("No valid video link")
 
         logger.info("Program ID: %s" % idEmission)
+
         # go for JSON straight, don't even try XML
-        jurl = self.JSON_DESCRIPTION.replace("_ID_EMISSION_", idEmission)
+        # jurl = self.JSON_DESCRIPTION.replace("_ID_EMISSION_", idEmission)
+        jurl = self.JSON3_DESC.replace("_ID_EMISSION_", idEmission)
         pageInfos = self.fakeAgent.readPage(jurl)
         
         # parse Metadata
         try:
           data = json.loads(pageInfos)
-          data["videoUrl"] = url
+          data["synopsis"] = self.getSynopsis(page)
+          data["videoUrl"] = videoUrl
+          data["ChannelUrl"] = url
           data["videoId"] = jurl
           videoMetadata = FranceTvVideoMetadata(data)
           metadata = videoMetadata.getMetadata()
