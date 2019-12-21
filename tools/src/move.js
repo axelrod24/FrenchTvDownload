def6c8a4cd19cc4255d38ba1a810d41f602b07cf
axelrod <axel.rod@mailinator.com>
2019-12-21 18:50:05 +0800

    add moving logic

--- tools/src/move.js
+++ tools/src/move.js
@@ -13,7 +13,7 @@ function moveFile(file, dir2) {
   var f = path.basename(file);
   var dest = path.resolve(dir2, f);
 
-  fs.rename(file, dest, (err)=>{
+  fs.renameSync(file, dest, (err)=>{
     if(err) throw err;
     else console.log('Successfully moved');
   });
@@ -22,10 +22,21 @@ function moveFile(file, dir2) {
 function processEachEntry(video) {
   const folder = video.folder
   const filename = video.filename+".mp4"
-  const srcPath = path.join(localcfg.SRC_BASE_PATH,folder,filename)
-  const dstPath = path.join(localcfg.DST_BASE_PATH,folder,filename)
+  const metafilename = video.filename+".meta"
+  console.log("Processing:"+folder+"/"+filename)
 
-  console.log("moving:"+srcPath+" to "+dstPath)
+  const srcPath = path.join(localcfg.SRC_BASE_PATH,folder,filename)
+  const srcPathMeta = path.join(localcfg.SRC_BASE_PATH,folder,metafilename)
+  const dstPath = path.join(localcfg.DST_BASE_PATH,folder)
+
+  if (!fs.existsSync(dstPath)) {
+    console.log("Creating:"+dstPath)
+    fs.mkdirSync(dstPath)
+  }
+
+  fs.renameSync(srcPath, path.join(dstPath, filename))
+  fs.renameSync(srcPathMeta, path.join(dstPath, metafilename))
+  console.log("moved:"+srcPath+" to "+dstPath)
 }
 
 dbapi.getAllVideo().then(videos => videos.forEach(processEachEntry))
