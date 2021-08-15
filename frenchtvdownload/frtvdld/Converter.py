03828877b7cdc516b1b756ed5dc958e1e7a56f42
axelrod24 <axel.rod@mailinator.com>
2021-08-15 10:27:24 +0200

    revert generation of ffmpeg log file

--- frenchtvdownload/frtvdld/Converter.py
+++ frenchtvdownload/frtvdld/Converter.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import subprocess
 
 from frtvdld.DownloadException import FrTvDownloadException
 from frtvdld.GlobalRef import LOGGER_NAME
@@ -32,13 +33,20 @@ class FfmpegHLSDownloader(object):
     self.userAgent = userAgent
 
   def downloadAndConvertFile(self, dst):
-    command = 'ffmpeg -hide_banner -user_agent "%s" -loglevel info -i "%s" -acodec copy -vcodec copy %s &> %s' % (self.userAgent, self.manifestUrl , dst, dst.split(".mp4")[0]+".log")
+    log_file = dst.split(".mp4")[0]+".log"
+    command = 'ffmpeg -hide_banner -user_agent "%s" -loglevel info -i "%s" -acodec copy -vcodec copy %s' % (self.userAgent, self.manifestUrl , dst)
+    #command = ['ffmpeg', '-hide_banner', '-user_agent', '%s'%(self.userAgent), '-loglevel', 'info', '-i', '%s'%(self.manifestUrl), '-acodec', 'copy', '-vcodec', 'copy', dst]
+    logger.info(command)
     try:
+      #with open(log_file, 'w') as stdout_file:
+      #  process_output = subprocess.run(command, stdout=stdout_file, stderr=subprocess.STDOUT, text=True)
+
       if (os.system(command) == 0):
+      #if (process_output.stderr==None):
         logger.info("-> %s" % dst)
       else:
-        logger.warning("Problem with FFmpeg")
-        raise FrTvDownloadException("Error running FFmpeg")
+        logger.warning("Problem with FFmpeg: %s"%process_output.stderr)
+        raise FrTvDownloadException("Error running: FFmpeg %s"%process_output.stderr)
       
     except Exception as err:
       raise FrTvDownloadException("Can't create final MP4. "+err.__repr__())
