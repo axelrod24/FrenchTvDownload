1bc16df058cd9130df95e69d9c426983e9a606dd
axelrod24 <axel.rod@mailinator.com>
2022-02-02 15:46:38 +0800

    fix tf1 parsing (videoid)

--- frenchtvdownload/downloadStream.py
+++ frenchtvdownload/downloadStream.py
@@ -2,8 +2,9 @@
 # # -*- coding:Utf-8 -*-
 
 import os
+
 v_env_base_path = os.environ.get('VIRTUAL_ENV', '../.venv')
-activate_this = os.path.join(v_env_base_path,'bin/activate_this.py')
+activate_this = os.path.join(v_env_base_path, 'bin/activate_this.py')
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
@@ -33,7 +34,7 @@ from urllib.parse import urlparse
 
 from frtvdld.DownloadException import *
 from frtvdld.ColorFormatter import ColorFormatter
-from frtvdld.network.NetworkProgParser import networkParserFactory 
+from frtvdld.network.NetworkProgParser import networkParserFactory
 from frtvdld.downloader.HLSDownloader import HlsManifestParser, HLSStreamDownloader
 
 from frtvdld.FakeAgent import FakeAgent, CURRENT_USER_AGENT
@@ -42,91 +43,96 @@ from frtvdld.GlobalRef import LOGGER_NAME
 
 from db.mongoapi import addStream, getStreamsByStatus, updateStreamWithError, updateStreamWithMetadata, addVideo, updateStreamStatus, getStreamByUrl
 
+
 def downloadOneStream(stream, args):
-  try:
-    url = stream.url
-    networkParser = networkParserFactory(stream.url)
-    progMetadata = networkParser.getProgMetaData(url)  
+    try:
+        url = stream.url
+        networkParser = networkParserFactory(url)
+        progMetadata = networkParser.getProgMetaData(url)
 
-  except Exception as err:
-    # log the error and continue with next stream
-    logger.error(err.__repr__())
-    updateStreamWithError(stream, err.__repr__())
-    return
+    except Exception as err:
+        # log the error and continue with next stream
+        logger.error(err.__repr__())
+        updateStreamWithError(stream, err.__repr__())
+        return
 
-  # create the filename accoding to file meta-data
-  # generic video filename (without ext)
-  folder = datetime.fromtimestamp(progMetadata.airDate).strftime("%Y-%m-%d_%a")
-  dstFullPath = os.path.join(args.outDir, folder)
+    # create the filename accoding to file meta-data
+    # generic video filename (without ext)
+    folder = datetime.fromtimestamp(
+        progMetadata.airDate).strftime("%Y-%m-%d_%a")
+    dstFullPath = os.path.join(args.outDir, folder)
 
-  # create dest folder if doesn't exist
-  if os.path.exists(dstFullPath) is False:
-    os.mkdir(dstFullPath)
+    # create dest folder if doesn't exist
+    if os.path.exists(dstFullPath) is False:
+        os.mkdir(dstFullPath)
 
-  # add filename
-  dstFullPath = os.path.join(dstFullPath, progMetadata.filename)
+    # add filename
+    dstFullPath = os.path.join(dstFullPath, progMetadata.filename)
 
-  # rename file if it already exist.
-  fileIndex = 2
-  while os.path.isfile(dstFullPath + ".mp4") is True:
-    dstFullPath = os.path.join(args.outDir, folder, progMetadata.filename + "_" + str(fileIndex))
-    fileIndex += 1
+    # rename file if it already exist.
+    fileIndex = 2
+    while os.path.isfile(dstFullPath + ".mp4") is True:
+        dstFullPath = os.path.join(
+            args.outDir, folder, progMetadata.filename + "_" + str(fileIndex))
+        fileIndex += 1
 
-  # downloading with ffmpeg
-  logger.info("------------")
-  logger.info("Downloading: %s" % (url))
-  logger.info("++")
-  updateStreamStatus(stream, "downloading")
-
-  try:
-    ffmpegHLSDownloader = FfmpegHLSDownloader(url=progMetadata.manifestUrl, userAgent=CURRENT_USER_AGENT)
-    ffmpegHLSDownloader.downloadAndConvertFile(dst=dstFullPath + ".mp4")
-  except Exception as err:
-    # log the error and continue with next stream
-    logger.error(err.__repr__())
-    updateStreamWithError(stream, err.__repr__())
-    logger.info("++")
-    logger.info("Download interupted with error")
-    logger.info("-------")
-    return 
-
-   # check log file for error
-  logger.info("------------")
-  logger.info(f"Checking log file: {dstFullPath}.log")
-  logger.info("++")
-  with open(dstFullPath+".log", 'r') as f:
-    lines = f.readlines()
-  if any(("error" in l for l in lines)):
+    # downloading with ffmpeg
     logger.info("------------")
-    logger.info("Found downloading error. Clean up and will retry later")
+    logger.info("Downloading: %s" % (url))
     logger.info("++")
-    cleanUp(dstFullPath)
-    updateStreamWithError(stream, "Segment download error")
-    updateStreamStatus(stream, "pending")
-    return 
+    updateStreamStatus(stream, "downloading")
 
-  logger.info("++")
-  logger.info("Download completed: %s" % (url))
-  logger.info("Video : %s" % (dstFullPath))
-  logger.info("-------")
+    try:
+        ffmpegHLSDownloader = FfmpegHLSDownloader(url=progMetadata.manifestUrl,
+                                                  userAgent=CURRENT_USER_AGENT)
+        ffmpegHLSDownloader.downloadAndConvertFile(dst=dstFullPath + ".mp4")
+    except Exception as err:
+        # log the error and continue with next stream
+        logger.error(err.__repr__())
+        updateStreamWithError(stream, err.__repr__())
+        logger.info("++")
+        logger.info("Download interupted with error")
+        logger.info("-------")
+        return
 
-  # save metadata
-  if (args.saveMetadata):
-    logger.info("Saving metadata")
-    xmlMeta = dicttoxml.dicttoxml(progMetadata._asdict(), attr_type=False)
-    dom = minidom.parseString(xmlMeta)
-    with open(dstFullPath+".meta", "w") as text_file:
-      print(dom.toprettyxml(), file=text_file)
+    # check log file for error
+    logger.info("------------")
+    logger.info(f"Checking log file: {dstFullPath}.log")
+    logger.info("++")
+    with open(dstFullPath + ".log", 'r') as f:
+        lines = f.readlines()
+    if any(("error" in l for l in lines)):
+        logger.info("------------")
+        logger.info("Found downloading error. Clean up and will retry later")
+        logger.info("++")
+        cleanUp(dstFullPath)
+        updateStreamWithError(stream, "Segment download error")
+        updateStreamStatus(stream, "pending")
+        return
 
-  dstFullPath += ".mp4"
-  # save metadata to mongo
-  logger.info("Saving video info")
-  updateStreamWithMetadata(stream, progMetadata)
-  addVideo(dstFullPath, folder, args.repo, progMetadata, stream)
+    logger.info("++")
+    logger.info("Download completed: %s" % (url))
+    logger.info("Video : %s" % (dstFullPath))
+    logger.info("-------")
+
+    # save metadata
+    if (args.saveMetadata):
+        logger.info("Saving metadata")
+        xmlMeta = dicttoxml.dicttoxml(progMetadata._asdict(), attr_type=False)
+        dom = minidom.parseString(xmlMeta)
+        with open(dstFullPath + ".meta", "w") as text_file:
+            print(dom.toprettyxml(), file=text_file)
+
+    dstFullPath += ".mp4"
+    # save metadata to mongo
+    logger.info("Saving video info")
+    updateStreamWithMetadata(stream, progMetadata)
+    addVideo(dstFullPath, folder, args.repo, progMetadata, stream)
+
 
 def cleanUp(dstFullPath):
-  for ext in (".mp4",".log"):
-    os.remove(dstFullPath+ext) 
+    for ext in (".mp4", ".log"):
+        os.remove(dstFullPath + ext)
 
 
 #
@@ -134,84 +140,110 @@ def cleanUp(dstFullPath):
 #
 if (__name__ == "__main__"):
 
-  # Arguments de la ligne de commande
-  usage = "downloadStream.py [options]"
-  parser = argparse.ArgumentParser(usage=usage, description="Download video from FranceTv.fr")
-  # parser.add_argument("-b", "--progressbar", action="store_true", default=False,
-  #                     help='display download progress')
-  parser.add_argument("-v", "--verbose", action="store_true", default=False,
-                      help='dispay download information')
+    # Arguments de la ligne de commande
+    usage = "downloadStream.py [options]"
+    parser = argparse.ArgumentParser(
+        usage=usage, description="Download video from FranceTv.fr")
+    # parser.add_argument("-b", "--progressbar", action="store_true", default=False,
+    #                     help='display download progress')
+    parser.add_argument("-v",
+                        "--verbose",
+                        action="store_true",
+                        default=False,
+                        help='dispay download information')
 
-  parser.add_argument("-o", "--outDir", action="store", default=".", help='output folder (default .)')
+    parser.add_argument("-o",
+                        "--outDir",
+                        action="store",
+                        default=".",
+                        help='output folder (default .)')
 
-  # parser.add_argument("--noDuplicate", action='store_true', default=False, help="download video only if video id not in Mongo db")
-  parser.add_argument("--saveMetadata", action='store_true', default=False, help="save the video metadata in file (.meta)")
-  parser.add_argument("--repo", action='store', default='TV', help="define the default folder")
-  parser.add_argument("--nocolor", action='store_true', default=False, help='turn of color in terminal')
-  parser.add_argument("--version", action='version', version="FrenchTvDownloader %s" % (__version__))
-  parser.add_argument("-i", "--input", action="store", choices=['url', 'mongo'], default="mongo", help='input source url or mongo')
-  parser.add_argument("urlEmission", action="store", default="", nargs="?", help="URL of video to download")
+    # parser.add_argument("--noDuplicate", action='store_true', default=False, help="download video only if video id not in Mongo db")
+    parser.add_argument("--saveMetadata",
+                        action='store_true',
+                        default=False,
+                        help="save the video metadata in file (.meta)")
+    parser.add_argument("--repo",
+                        action='store',
+                        default='TV',
+                        help="define the default folder")
+    parser.add_argument("--nocolor",
+                        action='store_true',
+                        default=False,
+                        help='turn of color in terminal')
+    parser.add_argument("--version",
+                        action='version',
+                        version="FrenchTvDownloader %s" % (__version__))
+    parser.add_argument("-i",
+                        "--input",
+                        action="store",
+                        choices=['url', 'mongo'],
+                        default="mongo",
+                        help='input source url or mongo')
+    parser.add_argument("urlEmission",
+                        action="store",
+                        default="",
+                        nargs="?",
+                        help="URL of video to download")
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  # set logger
-  logger = logging.getLogger(LOGGER_NAME)
-  console = logging.StreamHandler(sys.stdout)
-  if (args.verbose):
-    logger.setLevel(logging.DEBUG)
-    console.setLevel(logging.DEBUG)
-  else:
-    logger.setLevel(logging.INFO)
-    console.setLevel(logging.INFO)
-  console.setFormatter(ColorFormatter(not args.nocolor))
-  logger.addHandler(console)
+    # set logger
+    logger = logging.getLogger(LOGGER_NAME)
+    console = logging.StreamHandler(sys.stdout)
+    if (args.verbose):
+        logger.setLevel(logging.DEBUG)
+        console.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+        console.setLevel(logging.INFO)
+    console.setFormatter(ColorFormatter(not args.nocolor))
+    logger.addHandler(console)
 
-  # Affiche des infos sur le systeme
-  logger.debug("Python %s (%s)" % (platform.python_version(), platform.machine()))
-  logger.debug("OS : %s %s" % (platform.system(), platform.version()))
+    # Affiche des infos sur le systeme
+    logger.debug("Python %s (%s)" %
+                 (platform.python_version(), platform.machine()))
+    logger.debug("OS : %s %s" % (platform.system(), platform.version()))
 
-  if args.input == "url":
-    url = args.urlEmission
-    parsedUrl = urlparse(url)
-    networkName = parsedUrl.hostname
-    programCode = "misc-program-code"
+    if args.input == "url":
+        url = args.urlEmission
+        parsedUrl = urlparse(url)
+        networkName = parsedUrl.hostname
+        programCode = "misc-program-code"
 
-    # check if stream exist
-    stream = getStreamByUrl(url)
-    if (stream):  #and stream.status=="done"):
-      logger.info("Duplicate. Continue ...")
-      logger.info("Url: %s" % (url))
-      logger.info("=" * 6)
-      exit(1)
+        # check if stream exist
+        stream = getStreamByUrl(url)
+        if (stream):  #and stream.status=="done"):
+            logger.info("Duplicate. Continue ...")
+            logger.info("Url: %s" % (url))
+            logger.info("=" * 6)
+            exit(1)
 
-    logger.info("Adding pending stream: %s" % (url))
-    stream = addStream(url, "pending")
-    stream.progCode = programCode
-    stream.networkName = networkName
-    stream.save()
-    logger.info("=" * 6)       
+        logger.info("Adding pending stream: %s" % (url))
+        stream = addStream(url, "pending")
+        stream.progCode = programCode
+        stream.networkName = networkName
+        stream.save()
+        logger.info("=" * 6)
 
-  # # fetch list of stream with error and exclude stream order than 2020/01/20
-  # d = datetime(2020, 1, 20)
-  # streams = getStreamsByStatus(status="error")
-  # for stream in streams:
-  #     if stream.dateAdded < d:
-  #         continue
-  #     errors = stream.lastErrors
-  #     if len(errors) >= 10
-  #     print(stream)
+    # # fetch list of stream with error and exclude stream order than 2020/01/20
+    # d = datetime(2020, 1, 20)
+    # streams = getStreamsByStatus(status="error")
+    # for stream in streams:
+    #     if stream.dateAdded < d:
+    #         continue
+    #     errors = stream.lastErrors
+    #     if len(errors) >= 10
+    #     print(stream)
 
+    streams = getStreamsByStatus(status="pending")
+    if not streams:
+        logger.info("-" * 6)
+        logger.info("No pending stream")
+        logger.info("=" * 6)
+        exit(1)
 
-  streams = getStreamsByStatus(status="pending")
-  if not streams:
-    logger.info("-" * 6)
-    logger.info("No pending stream")
-    logger.info("=" * 6)
-    exit(1)
+    for stream in streams:
+        downloadOneStream(stream, args)
 
-  for stream in streams:
-    downloadOneStream(stream, args)
-
-  logger.info("=========================================================")
-
-  
+    logger.info("=========================================================")
--- frenchtvdownload/frtvdld/network/Tf1Parser.py
+++ frenchtvdownload/frtvdld/network/Tf1Parser.py
@@ -16,29 +16,37 @@ from frtvdld.network.NetworkParser import NetworkParser, VideoMetadata
 
 logger = logging.getLogger(LOGGER_NAME)
 
+
 class Tf1VideoMetadata(VideoMetadata):
-  def __init__(self, d):
-    super().__init__(d)
 
-  def parseMetadata(self):
-    deliveryTag = self.get('delivery', {})
-    if deliveryTag.get("code") != 200:
-      raise FrTvDownloadException("Can't parse json url for Tf1")    
+    def __init__(self, d):
+        super().__init__(d)
 
-    self._manifestUrl = deliveryTag.get('url')
-    self._mediaType = deliveryTag.get("format")
+    def parseMetadata(self):
+        deliveryTag = self.get('delivery', {})
+        if deliveryTag.get("code") != 200:
+            raise FrTvDownloadException("Can't parse json url for Tf1")
 
-    self._progTitle = self.get('content', {}).get("title", "default_prog_title")
-    self._progName = self.get('media', {}).get('owner', "default_prog_name")
-    self._duration = self.get('media', {}).get('duration', 0)
+        self._manifestUrl = deliveryTag.get('url')
+        self._mediaType = deliveryTag.get("format")
 
-    data = self.get("mediametrie",{}).get("chapters",[{}])[0]
-    gregorian_date = data.get('estatS4')
-    self._airDate = time.mktime(datetime.strptime(gregorian_date, "%Y-%m-%d").timetuple()) 
-    self._synopsis = "%s\r\n%s" % (self.get('media', {}).get('title', ""), self.get('synopsis'))
+        self._progTitle = self.get('content', {}).get("title",
+                                                      "default_prog_title")
+        self._progName = self.get('media', {}).get('owner',
+                                                   "default_prog_name")
+        self._duration = self.get('media', {}).get('duration', 0)
 
-    self._videoId = self.get('videoId')
-    self._filename = "%s-Tf1-%s" % (datetime.fromtimestamp(self._airDate).strftime("%Y%m%d"), self.normalizeProgTitle(self._progTitle))
+        data = self.get("mediametrie", {}).get("chapters", [{}])[0]
+        gregorian_date = data.get('estatS4')
+        self._airDate = time.mktime(
+            datetime.strptime(gregorian_date, "%Y-%m-%d").timetuple())
+        self._synopsis = "%s\r\n%s" % (self.get('media', {}).get(
+            'title', ""), self.get('synopsis'))
+
+        self._videoId = self.get('videoId')
+        self._filename = "%s-Tf1-%s" % (
+            datetime.fromtimestamp(self._airDate).strftime("%Y%m%d"),
+            self.normalizeProgTitle(self._progTitle))
 
 
 class Tf1Parser(NetworkParser):
@@ -52,30 +60,30 @@ class Tf1Parser(NetworkParser):
     REPLAY_VIDEO_URL = "%s/videos/replay/%d"
 
     def parseCollection(self, baseUrl, nbrPage=1, nbrVideoLink=1):
-      index = 0
-      # read the page
-      listVideoUrl=[]
-      while(nbrPage==-1 or index < nbrPage):
-        url = self.REPLAY_VIDEO_URL % (baseUrl.rstrip('/'), index) 
-        page = self.fakeAgent.readPage(url)
-        if len(page)==0: # nothing on the page, that's the last one.
-          break
-        parsed = BeautifulSoup(page, "html.parser")
-        cardVideos = parsed.find_all("div", attrs={"class": "VideoCard_card__box_k3hxc"})
-        for card in cardVideos:
-          href = card.find_all("a")
-          if len(href) == 0:  # no link, weird but continue
-            continue
+        index = 0
+        # read the page
+        listVideoUrl = []
+        while (nbrPage == -1 or index < nbrPage):
+            url = self.REPLAY_VIDEO_URL % (baseUrl.rstrip('/'), index)
+            page = self.fakeAgent.readPage(url)
+            if len(page) == 0:  # nothing on the page, that's the last one.
+                break
+            parsed = BeautifulSoup(page, "html.parser")
+            cardVideos = parsed.find_all(
+                "div", attrs={"class": "VideoCard_card__box_k3hxc"})
+            for card in cardVideos:
+                href = card.find_all("a")
+                if len(href) == 0:  # no link, weird but continue
+                    continue
 
-          videoUrl = urljoin(baseUrl, href[0]["href"])
-          listVideoUrl.append(videoUrl)
-          if len(listVideoUrl) == nbrVideoLink:
-            return listVideoUrl
+                videoUrl = urljoin(baseUrl, href[0]["href"])
+                listVideoUrl.append(videoUrl)
+                if len(listVideoUrl) == nbrVideoLink:
+                    return listVideoUrl
 
-        index+=1
+            index += 1
 
-      return listVideoUrl
-
+        return listVideoUrl
 
     def parsePage(self, url):
 
@@ -92,83 +100,84 @@ class Tf1Parser(NetworkParser):
         pageInfos1 = self.fakeAgent.readPage(jurl1)
         # jurl2 = self.JSON_DESCRIPTION.replace("_ID_EMISSION_", idEmission)
         # pageInfos2 = self.fakeAgent.readPage(jurl2)
-        
-        try:
-          data = json.loads(pageInfos1)
-          # data.update(json.loads(pageInfos2))
-          data["videoUrl"] = url
-          data["videoId"] = idEmission
-          data["synopsis"] = self._getSynopsis(page)
 
-          videoMetadata = Tf1VideoMetadata(data)
-          metadata = videoMetadata.getMetadata()
+        try:
+            data = json.loads(pageInfos1)
+            # data.update(json.loads(pageInfos2))
+            data["videoUrl"] = url
+            data["videoId"] = idEmission
+            data["synopsis"] = self._getSynopsis(page)
+
+            videoMetadata = Tf1VideoMetadata(data)
+            metadata = videoMetadata.getMetadata()
         except Exception as e:
-          logger.error(e)
-          raise FrTvDwnMetaDataParsingError(e)
+            logger.error(e)
+            raise FrTvDwnMetaDataParsingError(e)
 
         self.progMetaData = metadata
 
     def _findProgPageMetadata(self, page):
-      try:
-        parsed = BeautifulSoup(page, "html.parser")
-        scripts = parsed.find_all("script")
-        if len(scripts) == 0:
+        try:
+            parsed = BeautifulSoup(page, "html.parser")
+            scripts = parsed.find_all("script")
+            if len(scripts) == 0:
+                return None
+
+            for script in scripts:
+                # extract stream id for var definition
+                if len(script.contents) > 0 and script.contents[0].startswith(
+                        "window.__APOLLO_STATE__"):
+                    return script
+                # if script.text.startswith("window.__APOLLO_STATE__"):
+                #   return script
+
             return None
 
-        for script in scripts:
-          # extract stream id for var definition
-          if len(script.contents)>0 and script.contents[0].startswith("window.__APOLLO_STATE__"):
-            return script
-          # if script.text.startswith("window.__APOLLO_STATE__"):
-          #   return script
-        
-        return None
-
-      except Exception as e:
-        logger.error(e)
-        raise FrTvDwnPageParsingError(e)
+        except Exception as e:
+            logger.error(e)
+            raise FrTvDwnPageParsingError(e)
 
     def _getVideoId(self, page):
-      """
+        """
       get Video ID from the video page
       """
-      try:
-        script = self._findProgPageMetadata(page)
-        script_text = script.contents[0] 
-        sindex = script_text.find('"streamId":')
-        eindex = sindex + script_text[sindex:].find(',')
-        val = script_text[sindex:eindex]
-        vid = val.split(':')[1].strip("'\" ")
-        return vid
+        try:
+            script = self._findProgPageMetadata(page)
+            script_text = script.contents[0]
+            # sindex = script_text.find('"streamId":')
+            pattern = '"__ref":"Video:'
+            sindex = script_text.find(pattern)
+            sindex += len(pattern)
+            eindex = sindex + script_text[sindex:].find('"')
+            val = script_text[sindex:eindex]
+            # vid = val.split(':')[1].strip("'\" ")
+            return val
 
-      except Exception as e:
-        logger.error(e)
-        raise FrTvDwnPageParsingError(e)
+        except Exception as e:
+            logger.error(e)
+            raise FrTvDwnPageParsingError(e)
 
     def _getSynopsis(self, page):
-      try:
-        script = self._findProgPageMetadata(page)
-        script_text = script.contents[0] 
-        sindex = script_text.find('"__typename":"Decoration"')
-        sindex = sindex + script_text[sindex:].find(',')
-        sindex = sindex + script_text[sindex:].find(':')
-        sindex = sindex + script_text[sindex:].find('"')
-        sindex+=1
-        eindex = sindex + script_text[sindex:].find('"')
+        try:
+            script = self._findProgPageMetadata(page)
+            script_text = script.contents[0]
+            sindex = script_text.find('"__typename":"Decoration"')
+            sindex = sindex + script_text[sindex:].find(',')
+            sindex = sindex + script_text[sindex:].find(':')
+            sindex = sindex + script_text[sindex:].find('"')
+            sindex += 1
+            eindex = sindex + script_text[sindex:].find('"')
 
-        val = script_text[sindex:eindex]
-        val = val.strip("'\" ")
-        return val
-          
-      except Exception as e:
-        logger.error(e)
-        raise FrTvDwnPageParsingError(e)
+            val = script_text[sindex:eindex]
+            val = val.strip("'\" ")
+            return val
 
-      parsed = BeautifulSoup(page, "html.parser")
-      desc = parsed.find_all("meta", attrs={"property": "og:description"})
-      if len(desc) > 0:
-        return desc[0].attrs["content"]
-      return "no_synopsis"
- 
+        except Exception as e:
+            logger.error(e)
+            raise FrTvDwnPageParsingError(e)
 
-
+        parsed = BeautifulSoup(page, "html.parser")
+        desc = parsed.find_all("meta", attrs={"property": "og:description"})
+        if len(desc) > 0:
+            return desc[0].attrs["content"]
+        return "no_synopsis"
